8aa0cb038dbc95ce6df03101a09905cddfcb61b4
Arpita <aabrol@stuy.edu>
2016-10-26 22:14:02 -0400

    finish

--- randfile.c
+++ randfile.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
 #include <sys/stat.h>
 
-char * get_seed_value() {
-
-}
 
 int randomize() {
   int *values;
   int file = open("/dev/random", O_RDONLY);
-  read(file, values, 4);
+  read(file, &values, 4);
+  srand(values);
   close(file);
-
-  srand(time(values));
   int rndnum = rand();
   
   return rndnum;
@@ -24,22 +21,36 @@ int randomize() {
 
 
 int main() {
-  printf("%d\n", randomize());
-  
+
+  //populating array of random nums
   int rand_nums[10];
   int i = 0;
-  //  while (i < 10) {
-  //rand_nums[i] = randomize();
-  //}
+  printf("Generating random numbers:\n");
+  while (i < 10) {
+    rand_nums[i] = randomize();
+    printf("\trandom %d: %d\n", i, rand_nums[i]);
+    i++;
+  }
+
+  //writing array to file
+  printf("Writing numbers to file...\n");
   umask(0000);
-  int write_file = open("rand_ints.txt", O_RDWR | O_CREAT, 0644);
-  write(write_file, rand_nums, sizeof(int) * 10);
-  //close(write_file);
-  
-  //  int rand_nums2[10];
-  //int read_file = open("rand_ints.txt", O_RDWR);
-  //read(read_file, rand_nums2, sizeof(int) * 10);
-  //close(read_file);
+  int write_file = open("rand_ints", O_WRONLY | O_CREAT, 0644);
+  write(write_file, rand_nums, 40);
+  close(write_file);
+
+  //reading array from file
+  printf("Reading numbers from file...\n");
+  int rand_nums2[10];
+  int read_file = open("rand_ints", O_RDONLY);
+  read(read_file, rand_nums2, 40);
+  close(read_file);
+
+  //checking numbers
+  printf("Verification that written values were the same:\n");
+  for( i=0; i<10; i++ ) {
+    printf("\trandom %d: %d\n", i, rand_nums2[i]);
+  }
   
   return 0;
 }
